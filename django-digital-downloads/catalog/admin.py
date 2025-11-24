dca3b0496d2b5b41b13382998420a646125e67c6
Chia-Codes <sanchian-l@hotmail.co.uk>
2025-11-24 18:12:55 +0000

    Register admin models for Product, DigitalAsset, and UserNote with appropriate configurations

--- django-digital-downloads/catalog/admin.py
+++ django-digital-downloads/catalog/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
+from .models import DigitalAsset, Product, UserNote
+
 # Register your models here.
+
+
+# PRODUCT ADMIN
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("title", "price_pennies", "active", "created_at")
+    list_filter = ("active",)
+    search_fields = ("title", "slug")
+    prepopulated_fields = {"slug": ("title",)}
+
+
+# DIGITAL ASSET ADMIN
+@admin.register(DigitalAsset)
+class DigitalAssetAdmin(admin.ModelAdmin):
+    list_display = ("file_name", "product", "size_bytes")
+
+
+# USER NOTE ADMIN
+@admin.register(UserNote)
+class UserNoteAdmin(admin.ModelAdmin):
+    list_display = ("title", "user", "product", "updated_at")
+    search_fields = ("title", "body")
