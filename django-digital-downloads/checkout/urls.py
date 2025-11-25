2d7cf9f4136e742b29fc6b24447fa08729de24c7
Chia-Codes <sanchian-l@hotmail.co.uk>
2025-11-25 20:02:39 +0000

    Add URL patterns and view for checkout app

--- django-digital-downloads/checkout/urls.py
+++ django-digital-downloads/checkout/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 
-from .views import start
+from .views import cancel, start, success, webhook
 
 app_name = "checkout"
-urlpatterns = [path("", start, name="start")]
+
+urlpatterns = [
+    path("", start, name="start"),
+    path("success/", success, name="success"),
+    path("cancel/", cancel, name="cancel"),
+    path("webhook/", webhook, name="webhook"),
+]
