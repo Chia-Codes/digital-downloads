73f94f38f2927192812f0b8c388b9ffcc8c23b3b
Chia-Codes <sanchian-l@hotmail.co.uk>
2025-11-26 16:32:50 +0000

    Create views and templates for managing user notes -sort isort error match Black

--- django-digital-downloads/orders/urls.py
+++ django-digital-downloads/orders/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 
-from .views import download_asset, order_detail, order_history, purchases
+from .views import (
+    download_asset,
+    note_create,
+    note_delete,
+    note_edit,
+    notes_list,
+    order_detail,
+    order_history,
+    purchases,
+)
 
 app_name = "orders"
 
@@ -9,4 +18,8 @@ urlpatterns = [
     path("<uuid:order_id>/", order_detail, name="detail"),
     path("downloads/", purchases, name="downloads"),
     path("download/<uuid:asset_id>/", download_asset, name="download"),
+    path("notes/", notes_list, name="notes_list"),
+    path("notes/create/<uuid:product_id>/", note_create, name="note_create"),
+    path("notes/<uuid:note_id>/edit/", note_edit, name="note_edit"),
+    path("notes/<uuid:note_id>/delete/", note_delete, name="note_delete"),
 ]
