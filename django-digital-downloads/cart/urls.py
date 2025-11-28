6c553601d720d84f1d542ac51f09680a8e47d0e2
Chia-Codes <sanchian-l@hotmail.co.uk>
2025-11-28 15:06:39 +0000

    Correct url path to cart detail

--- django-digital-downloads/cart/urls.py
+++ django-digital-downloads/cart/urls.py
@@ -6,7 +6,7 @@ app_name = "cart"
 
 urlpatterns = [
     path("", cart_view, name="view"),
-    path("add/", add_to_cart, name="add"),
-    path("update/", update_quantity, name="update"),
-    path("remove/", remove_from_cart, name="remove"),
+    path("add/<uuid:product_id>/", add_to_cart, name="add"),
+    path("update/<uuid:product_id>/", update_quantity, name="update"),
+    path("remove/<uuid:product_id>/", remove_from_cart, name="remove"),
 ]
