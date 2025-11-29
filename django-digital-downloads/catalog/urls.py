7c998679c2b4ab948e894b162fed621c72d70d46
Chia-Codes <sanchian-l@hotmail.co.uk>
2025-11-29 19:59:36 +0000

    Add health check for deployment

--- django-digital-downloads/catalog/urls.py
+++ django-digital-downloads/catalog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import product_detail, product_list
+from .views import catalog_health, product_detail, product_list
 
 app_name = "catalog"
 
 urlpatterns = [
     path("", product_list, name="list"),
+    path("health/", catalog_health, name="health"),
     path("<slug:slug>/", product_detail, name="detail"),
 ]
--- django-digital-downloads/catalog/views.py
+++ django-digital-downloads/catalog/views.py
@@ -1,6 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 
 from .models import Product
+
+
+# Health Check View
+def catalog_health(request):
+    qs = Product.objects.all()
+    act = Product.objects.filter(active=True)
+    return HttpResponse(
+        f"total={qs.count()} active={act.count()} slugs={list(act.values_list(
+            'slug', flat=True
+        ))}"
+    )
 
 
 # Product List View
