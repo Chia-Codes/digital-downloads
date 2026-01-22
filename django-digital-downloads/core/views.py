0131dbcff1029c68a8672652f4e5a99d9e80ed3b
Chia-Codes <sanchian-l@hotmail.co.uk>
2026-01-22 14:55:19 +0000

    Add 404 and 500 error handlers that redirect to home page

--- django-digital-downloads/core/views.py
+++ django-digital-downloads/core/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 
 def home(request):
     return render(request, "home.html")
+
+
+def not_found_redirect(request, exception=None):
+    return redirect("core:home")
+
+
+def server_error_redirect(request):
+    return redirect("core:home")
